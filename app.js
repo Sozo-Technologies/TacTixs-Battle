bf14ac25b31306fee1653f2496f4d731d93566ad
IzanamiiDevv <rafaeloli596@gmail.com>
2025-01-10 22:43:58 +0800

    Created a EntryPoint for Server

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const exp = require("constants");
 // const route = require("./api/routes");
 // const handler = require("./api/controller");
 
 const app = express();
 const port = 5000;
+const sourcePath = path.join(__dirname, "src");
+
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(sourcePath, "pages/HomePage.html"));
+});
 
 app.listen(port, () => {
     console.log(`Streaming on: http://localhost:${port}`);
