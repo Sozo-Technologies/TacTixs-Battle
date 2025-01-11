b93795ba2b12d5e5cb20779bc3e7af883e0a8af5
IzanamiiDevv <rafaeloli596@gmail.com>
2025-01-11 22:53:53 +0800

    Create a script

--- src/script/HomePage.js
+++ src/script/HomePage.js
@@ -1,4 +1,14 @@
 import Pages from "./Pages.js";
+import { P_Request } from "./Server.js"
+
 Pages.load((err, data) => {
-   
+   console.log(data);
+});
+
+
+document.getElementById("btn").addEventListener('click', () => {
+   const value = document.getElementById("typebox").value;
+   P_Request("/WriteTest", {data: value}, (response) => {
+      console.log(response);
+   });
 });
